a0f09b82b056972a1e7cb4823c1ef20a86d3eb4d
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-07-13 03:06:23 +0300

    will work on it

--- main.js
+++ main.js
@@ -2,7 +2,59 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+                                // Your JavaScript code goes here!
+                                document.addEventListener("DOMContentLoaded", () => {
+                                  const errorModal = document.getElementById("modal");
+                                  const errorMessage = document.getElementById("modal-message");
+
+                                  // Add the .hidden class to the error modal initially
+                                  errorModal.classList.add("hidden");
+
+                                  // Function to handle the server response
+                                  const handleServerResponse = (response) => {
+                                    if (response === "success") {
+                                      // Change the heart to a full heart
+                                      event.target.innerHTML = `Like! <span class="like-glyph activated-heart">&#x2665;</span>`;
+                                    } else {
+                                      // Display the error modal
+                                      errorModal.classList.remove("hidden");
+                                      errorMessage.textContent = "Server Error. Please try again.";
+
+                                      // Hide the modal after 3 seconds
+                                      setTimeout(() => {
+                                        errorModal.classList.add("hidden");
+                                      }, 3000);
+                                    }
+                                  };
+
+                                  // Function to handle the like action
+                                  const handleLikeAction = (event) => {
+                                    const likeGlyph = event.target;
+
+                                    // Simulate server request
+                                    mimicServerCall()
+                                      .then((response) => handleServerResponse(response))
+                                      .catch(() => {
+                                        // Display the error modal
+                                        errorModal.classList.remove("hidden");
+                                        errorMessage.textContent = "Network Error. Please try again.";
+
+                                        // Hide the modal after 3 seconds
+                                        setTimeout(() => {
+                                          errorModal.classList.add("hidden");
+                                        }, 3000);
+                                      });
+
+                                    // Toggle heart state
+                                    likeGlyph.classList.toggle("activated-heart");
+                                  };
+
+  // Attach event listener to each like button
+  const likeButtons = document.querySelectorAll(".like");
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", handleLikeAction);
+  });
+});
 
 
 
